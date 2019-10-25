9d00cf0cfddbcc58d1230fe59d33b5e3a86e3756
sbienaime <stevebienaime@gmail.com>
2019-10-25 05:50:31 -0400

    initial commit

---
+++ lib/internal/Ebay/Autoloader.php
@@ -0,0 +1,16 @@
+<?php
+include "eBayAmountType.php";
+include "eBayAuth.php";
+
+include "eBayCredentials.php";
+include "eBayFeeType.php";
+
+include "eBayFeesType.php";
+include "eBayGetSearchResultsResponseType.php";
+
+include "eBaySOAP.php";
+include "eBaySearchResultItemArrayType.php";
+
+include "eBaySearchResultItemType.php";
+include "eBaySession.php";
+include "eBayUtils.php";
